3755b8ddbd5a39c5a3decbd59ee1ce6e5968afa5
Naman Gupta <naman19436@iiitd.ac.in>
2024-02-06 19:33:18 +0530

    first commit

---
+++ test_model.py
@@ -0,0 +1,278 @@
+
+from __future__ import print_function
+
+
+import argparse
+import os
+import shutil
+import time
+import random
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+import models.wideresnet as models
+import dataset.svhn as dataset
+from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
+from tensorboardX import SummaryWriter
+
+PATH = "/home/aml-group09/folder1/MixMatch-pytorch/result1/model_best.pth.tar"
+
+parser = argparse.ArgumentParser(description='PyTorch MixMatch Training')
+# Optimization options
+parser.add_argument('--epochs', default=1024, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--batch-size', default=64, type=int, metavar='N',
+                    help='train batchsize')
+parser.add_argument('--lr', '--learning-rate', default=0.002, type=float,
+                    metavar='LR', help='initial learning rate')
+# Checkpoints
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+# Miscs
+parser.add_argument('--manualSeed', type=int, default=0, help='manual seed')
+#Device options
+parser.add_argument('--gpu', default='0', type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')
+#Method options
+parser.add_argument('--n-labeled', type=int, default=250,
+                        help='Number of labeled data')
+parser.add_argument('--train-iteration', type=int, default=1024,
+                        help='Number of iteration per epoch')
+parser.add_argument('--out', default='result',
+                        help='Directory to output the result')
+parser.add_argument('--alpha', default=0.75, type=float)
+parser.add_argument('--lambda-u', default=75, type=float)
+parser.add_argument('--T', default=0.5, type=float)
+parser.add_argument('--ema-decay', default=0.999, type=float)
+
+
+args = parser.parse_args()
+# wandb.init(project="test-project", entity="chads")
+state = {k: v for k, v in args._get_kwargs()}
+# wandb.config = state
+
+
+# Use CUDA
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+use_cuda = torch.cuda.is_available()
+
+# Random seed
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+np.random.seed(args.manualSeed)
+
+best_acc = 0  # best test accuracy
+
+def main():
+    global best_acc
+
+    if not os.path.isdir(args.out):
+        mkdir_p(args.out)
+
+    # Data
+    print(f'==> Preparing svhn')
+    transform_train = transforms.Compose([
+        dataset.RandomPadandCrop(32),
+        dataset.RandomFlip(),
+        dataset.ToTensor(),
+    ])
+
+    transform_val = transforms.Compose([
+        dataset.ToTensor(),
+    ])
+
+    train_labeled_set, train_unlabeled_set, val_set, test_set = dataset.get_svhn('./data', args.n_labeled, transform_train=transform_train, transform_val=transform_val)
+    labeled_trainloader = data.DataLoader(train_labeled_set, batch_size=args.batch_size, shuffle=True, num_workers=0, drop_last=True)
+    unlabeled_trainloader = data.DataLoader(train_unlabeled_set, batch_size=args.batch_size, shuffle=True, num_workers=0, drop_last=True)
+    val_loader = data.DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    test_loader = data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=0)
+
+    # Model
+    print("==> creating WRN-28-2")
+
+    def create_model(ema=False):
+        model = models.WideResNet(num_classes=10)
+        model = model.cuda()
+
+        if ema:
+            for param in model.parameters():
+                param.detach_()
+
+        return model
+
+    model = create_model()
+    ema_model = create_model(ema=True)
+
+    cudnn.benchmark = True
+    print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
+
+    train_criterion = SemiLoss()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    ema_optimizer= WeightEMA(model, ema_model, alpha=args.ema_decay)
+    start_epoch = 0
+    # Resume
+    title = 'noisy-cifar-10'
+    if args.resume:
+        # Load checkpoint.
+        print("hello world")
+        print('==> Resuming from checkpoint..')
+        assert os.path.isfile(args.resume), 'Error: no checkpoint directory found!'
+        args.out = os.path.dirname(args.resume)
+        checkpoint = torch.load(args.resume)
+        best_acc = checkpoint['best_acc']
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        ema_model.load_state_dict(checkpoint['ema_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        logger = Logger(os.path.join(args.out, 'log.txt'), title=title, resume=True)
+    else:
+        logger = Logger(os.path.join(args.out, 'log.txt'), title=title)
+        logger.set_names(['Train Loss', 'Train Loss X', 'Train Loss U',  'Valid Loss', 'Valid Acc.', 'Test Loss', 'Test Acc.'])
+
+    writer = SummaryWriter(args.out)
+    step = 0
+    test_accs = []
+    # Train and val
+    
+
+    test_loss, test_acc = validate(test_loader, ema_model, criterion, 1, use_cuda, mode='Test Stats ')
+    print(test_loss,test_acc)
+
+       
+
+def validate(valloader, model, criterion, epoch, use_cuda, mode):
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    # bar = Bar(f'{mode}', max=len(valloader))
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(valloader):
+            # measure data loading time
+            data_time.update(time.time() - end) 
+
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda(non_blocking=True)
+            # compute output
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(outputs, targets, topk=(1, 5))
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(prec1.item(), inputs.size(0))
+            top5.update(prec5.item(), inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            # plot progress
+        #     bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+        #                 batch=batch_idx + 1,
+        #                 size=len(valloader),
+        #                 data=data_time.avg,
+        #                 bt=batch_time.avg,
+        #                 total=bar.elapsed_td,
+        #                 eta=bar.eta_td,
+        #                 loss=losses.avg,
+        #                 top1=top1.avg,
+        #                 top5=top5.avg,
+        #                 )
+        #     bar.next()
+        # bar.finish()
+    return (losses.avg, top1.avg)
+
+def save_checkpoint(state, is_best, checkpoint=args.out, filename='checkpoint.pth.tar'):
+    filepath = os.path.join(checkpoint, filename)
+    torch.save(state, filepath)
+    if is_best:
+        shutil.copyfile(filepath, os.path.join(checkpoint, 'model_best.pth.tar'))
+
+def linear_rampup(current, rampup_length=args.epochs):
+    if rampup_length == 0:
+        return 1.0
+    else:
+        current = np.clip(current / rampup_length, 0.0, 1.0)
+        return float(current)
+
+class SemiLoss(object):
+    def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch):
+        probs_u = torch.softmax(outputs_u, dim=1)
+
+        Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        Lu = torch.mean((probs_u - targets_u)**2)
+
+        return Lx, Lu, args.lambda_u * linear_rampup(epoch)
+
+class WeightEMA(object):
+    def __init__(self, model, ema_model, alpha=0.999):
+        self.model = model
+        self.ema_model = ema_model
+        self.alpha = alpha
+        self.params = list(model.state_dict().values())
+        self.ema_params = list(ema_model.state_dict().values())
+        self.wd = 0.02 * args.lr
+
+        for param, ema_param in zip(self.params, self.ema_params):
+            param.data.copy_(ema_param.data)
+
+    def step(self):
+        one_minus_alpha = 1.0 - self.alpha
+        for param, ema_param in zip(self.params, self.ema_params):
+            if ema_param.dtype==torch.float32:
+                ema_param.mul_(self.alpha)
+                ema_param.add_(param * one_minus_alpha)
+                # customized weight decay
+                param.mul_(1 - self.wd)
+
+def interleave_offsets(batch, nu):
+    groups = [batch // (nu + 1)] * (nu + 1)
+    for x in range(batch - sum(groups)):
+        groups[-x - 1] += 1
+    offsets = [0]
+    for g in groups:
+        offsets.append(offsets[-1] + g)
+    assert offsets[-1] == batch
+    return offsets
+
+
+def interleave(xy, batch):
+    nu = len(xy) - 1
+    offsets = interleave_offsets(batch, nu)
+    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
+    for i in range(1, nu + 1):
+        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
+    return [torch.cat(v, dim=0) for v in xy]
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
